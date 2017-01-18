b48c48f7ac14f5265823347afb1acf95720a645f
yeffasol <yeffasol@mail.ru>
2017-01-18 20:30:03 +0300

    Add all elements

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,13 @@
-const name = 4;
+(function() {
+  let toggler = document.querySelector(".menu__toggle");
+
+  toggler.addEventListener("click", function() {
+    document.querySelector(".menu__list").classList.toggle("menu__list--hide");
+    this.classList.toggle("menu__toggle--close");
+  });
+})();
+
+(function() {
+
+
+})();
